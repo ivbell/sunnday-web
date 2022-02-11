f64bc214bd5adbb3fbcb8309f8f7eac7cdb3c5e8
Ivan Bellokrylov <77839690+ivbell@users.noreply.github.com>
2022-02-11 14:35:47 +0400

    del: - task route

--- src/components/common/navbar-list/NavbarListItemArrowFolder.tsx
+++ src/components/common/navbar-list/NavbarListItemArrowFolder.tsx
@@ -1,9 +1,8 @@
 import { Box, Icon, Stack, Text } from '@chakra-ui/react'
 import React, { FC, useState } from 'react'
+import { MdKeyboardArrowDown, MdKeyboardArrowUp } from 'react-icons/md'
 import { TaskFolder } from '../../../lib/hooks/data/useTaskFolder'
-import { RouterLink } from '../RouterLink'
 import NavbarListItemCountLink from './NavbarListItemCountLink'
-import { MdKeyboardArrowUp, MdKeyboardArrowDown } from 'react-icons/md'
 
 interface Props {
   title: string
@@ -15,34 +14,38 @@ interface Props {
 const NavbarListItemArrowFolder: FC<Props> = (props) => {
   const [isShow, setIsShow] = useState<boolean>(false)
 
-  const { title, folder_list, to, count } = props
+  const { title, folder_list, count } = props
   const folderList = folder_list?.map((r) => (
-    <Box _hover={{ backgroundColor: 'sn.bg_accent' }} pl={12} py={1} pr={2} key={r._id}>
+    <Box
+      _hover={{ backgroundColor: 'sn.bg_accent' }}
+      pl={12}
+      py={1}
+      pr={2}
+      key={r._id}>
       <NavbarListItemCountLink title={r.name} count={r.count} to={r._id} />
     </Box>
   ))
 
   return (
     <Box position={'relative'}>
-      <RouterLink to={`/task/folder/${to}`}>
-        <Stack
+      <Stack
         _hover={{ backgroundColor: 'sn.bg_accent' }}
-          pl={9}
-          py={1}
-          pr={2}
-          cursor={'pointer'}
-          direction={['column', 'row']}
-          justify={'space-between'}>
-          <Text>{title}</Text>
-          <Text fontSize={'sm'}>{count}</Text>
-        </Stack>
-      </RouterLink>
+        pl={9}
+        py={1}
+        pr={2}
+        cursor={'pointer'}
+        direction={['column', 'row']}
+        alignItems={'center'}
+        justify={'space-between'}>
+        <Text fontSize={'sm'}>{title}</Text>
+        <Text fontSize={'xs'}>{count}</Text>
+      </Stack>
       <Icon
         _hover={{ color: 'sn.accent' }}
         position={'absolute'}
         left={2}
         cursor={'pointer'}
-        top={1}
+        top={0.5}
         onClick={() => setIsShow(!isShow)}
         w={6}
         h={6}
