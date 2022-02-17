cd6a7052de8f8ea8e1c16b824c4994a2136d9438
Ivan Bellokrylov <77839690+ivbell@users.noreply.github.com>
2022-02-17 13:54:56 +0400

    23

--- src/lib/store/reducers/user/UserSlice.ts
+++ src/lib/store/reducers/user/UserSlice.ts
@@ -1,41 +1,38 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit'
-import {
-  userAuthToken,
-  UserIdResponseToken,
-} from '../../action-creators/user.action'
+import { UserIdResponseToken } from '../../action-creators/user.action'
 
 export interface UserState {
-  readonly user_id: string
-  readonly loading: boolean
-  readonly is_auth: boolean
+    readonly user_id: string
+    readonly loading: boolean
+    readonly is_auth: boolean
 }
 
 const initialState: UserState = {
-  is_auth: false,
-  loading: false,
-  user_id: '',
+    is_auth: false,
+    loading: false,
+    user_id: '',
 }
 
 export const userSlice = createSlice({
-  name: 'user',
-  initialState,
-  reducers: {
-    isAuth(state) {
-      state.loading = true
+    name: 'user',
+    initialState,
+    reducers: {
+        isAuth(state) {
+            state.loading = true
+        },
+        isAuthSuccess(state, action: PayloadAction<UserIdResponseToken>) {
+            state.loading = false
+            state.is_auth = action.payload.auth
+            state.user_id = action.payload.user_id
+        },
+        isAuthError(state) {
+            state.loading = false
+        },
+        isLogout(state) {
+            state.user_id = ''
+            state.is_auth = false
+        },
     },
-    isAuthSuccess(state, action: PayloadAction<UserIdResponseToken>) {
-      state.loading = false
-      state.is_auth = action.payload.auth
-      state.user_id = action.payload.user_id
-    },
-    isAuthError(state) {
-      state.loading = false
-    },
-    isLogout(state) {
-      state.user_id = ''
-      state.is_auth = false
-    },
-  },
 })
 
 export default userSlice.reducer
